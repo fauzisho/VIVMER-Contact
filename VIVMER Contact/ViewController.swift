d1aea46958d48f48565c45aff2d8281a9e672e6e
fauzisho <fauzisholichin@gmail.com>
2019-04-30 14:57:52 +0800

    Initial Commit

---
+++ VIVMER Contact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIVMER Contact
+//
+//  Created by UziApel on 30/04/19.
+//  Copyright © 2019 UziApel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
